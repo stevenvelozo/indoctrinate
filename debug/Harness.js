7c6af2e924e7a2481304c3fe62389579a801685b
Steven Velozo <steven@velozo.com>
2025-03-20 21:17:58 -0700

    Changes to Harness

--- debug/Harness.js
+++ debug/Harness.js
@@ -6,10 +6,11 @@ let libIndoctrinate = require('../source/Indoctrinate-CLIProgram.js');
 
 // The folder to read
 let tmpInputFolder = `${__dirname}/../`;
-tmpInputFolder = `${__dirname}/../docs/examples/book/`;
+//tmpInputFolder = `${__dirname}/../docs/examples/book/`;
 //tmpInputFolder = `${__dirname}/../examples/data_model/`;
 //tmpInputFolder = `${__dirname}/../examples/document/`;
-//tmpInputFolder = `${__dirname}/../examples/monorepo/`;
+//tmpInputFolder = `${__dirname}/../../`;
+tmpInputFolder = `${__dirname}/../examples/monorepo/`;
 //tmpInputFolder = `${__dirname}/tmp/`;
 tmpInputFolder = libPath.resolve(tmpInputFolder);
 
@@ -17,5 +18,5 @@ tmpInputFolder = libPath.resolve(tmpInputFolder);
 let tmpTargetOutputFolder = `${__dirname}/dist/`;
 tmpTargetOutputFolder = libPath.resolve(tmpTargetOutputFolder);
 
-libIndoctrinate.run(['node', 'Harness.js', 'compile', '-d', tmpInputFolder, '-t', tmpTargetOutputFolder, '-c' ]);
+libIndoctrinate.run(['node', 'Harness.js', 'compile', '-d', tmpInputFolder, '-t', tmpTargetOutputFolder, '-c']);
 //libIndoctrinate.run(['node', 'Harness.js', 'extended_process', 'dist/indoctrinate_content_staging/Indoctrinate-Catalog-AppData.json' ]);
